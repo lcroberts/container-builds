1bf6a1359c81ccc52987a65333091c0e4d6c2272
Logan Roberts <logan@lcroberts.dev>
2025-01-06 08:42:30 -0500

    Refactor build scripts

---
+++ scripts/wmbazzite-functions.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+install-cosmic() {
+    curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
+    rpm-ostree --idempotent install cosmic-desktop
+}
+
+install-hyprland() {
+    curl -Lo /etc/yum.repos.d/_copr_solopasha-hypr.repo https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-"${RELEASE}"/solopasha-hyprland-fedora-"${RELEASE}".repo
+    rpm-ostree --idempotent install \
+        hyprland \
+        hyprland-contrib \
+        hyprpicker \
+        hyprlock \
+        hypridle \
+        hyprpolkitagent \
+        hyprsunset \
+        rofi-wayland \
+        swww \
+        aylurs-gtk-shell \
+        network-manager-applet \
+        wl-clipboard \
+        brightnessctl \
+        playerctl \
+        kanshi \
+        pavucontrol \
+        blueman
+}
+
+install-kmonad() {
+    curl -Lo /usr/bin/kmonad https://github.com/kmonad/kmonad/releases/latest/download/kmonad
+    chmod +x /usr/bin/kmonad
+}
+
+install-vscode() {
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" >/etc/yum.repos.d/vscode.repo
+    rpm-ostree --idempotent install code
+}
+
+install-docker() {
+    curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
+    rpm-ostree --idempotent install \
+        docker-ce \
+        docker-ce-cli \
+        docker-buildx-plugin \
+        docker-compose-plugin \
+        containerd.io
+}
