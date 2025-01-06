4136276c561bc30299f6d583049f76151ea53c60
Logan Roberts <logan@lcroberts.dev>
2025-01-05 20:03:08 -0500

    Reenable everything

--- wmbazzite-gnome/build.sh
+++ wmbazzite-gnome/build.sh
@@ -14,62 +14,61 @@ curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedor
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" >/etc/yum.repos.d/vscode.repo
 
-# this installs a package from fedora repos
-# rpm-ostree --idempotent install \
-#     adobe-source-han-sans-jp-fonts \
-#     adobe-source-han-serif-jp-fonts \
-#     mozilla-fira-mono-fonts \
-#     powerline-fonts \
-#     jetbrains-mono-fonts-all \
-#     google-droid-sans-mono-fonts \
-#     zathura \
-#     kitty \
-#     imv \
-#     zathura-plugins-all \
-#     fcitx5 \
-#     fcitx5-configtool \
-#     fcitx5-gtk \
-#     fcitx5-qt5 \
-#     fcitx5-mozc \
-#     fish \
-#     neovim \
-#     eza \
-#     fd-find \
-#     ripgrep \
-#     zoxide \
-#     trash-cli \
-#     tmux \
-#     iotop \
-#     tokei \
-#     tealdeer \
-#     yq \
-#     piper \
-#     code \
-#     docker-ce \
-#     docker-ce-cli \
-#     docker-buildx-plugin \
-#     docker-compose-plugin \
-#     containerd.io
+rpm-ostree --idempotent install \
+    adobe-source-han-sans-jp-fonts \
+    adobe-source-han-serif-jp-fonts \
+    mozilla-fira-mono-fonts \
+    powerline-fonts \
+    jetbrains-mono-fonts-all \
+    google-droid-sans-mono-fonts \
+    zathura \
+    kitty \
+    imv \
+    zathura-plugins-all \
+    fcitx5 \
+    fcitx5-configtool \
+    fcitx5-gtk \
+    fcitx5-qt5 \
+    fcitx5-mozc \
+    fish \
+    neovim \
+    eza \
+    fd-find \
+    ripgrep \
+    zoxide \
+    trash-cli \
+    tmux \
+    iotop \
+    tokei \
+    tealdeer \
+    yq \
+    piper \
+    code \
+    docker-ce \
+    docker-ce-cli \
+    docker-buildx-plugin \
+    docker-compose-plugin \
+    containerd.io
 
 # Hyprland Stuff
-# rpm-ostree --idempotent install \
-#     hyprland \
-#     hyprland-contrib \
-#     hyprpicker \
-#     hyprlock \
-#     hypridle \
-#     hyprpolkitagent \
-#     hyprsunset \
-#     rofi-wayland \
-#     swww \
-#     aylurs-gtk-shell \
-#     network-manager-applet \
-#     wl-clipboard \
-#     brightnessctl \
-#     playerctl \
-#     kanshi \
-#     pavucontrol \
-#     blueman
+rpm-ostree --idempotent install \
+    hyprland \
+    hyprland-contrib \
+    hyprpicker \
+    hyprlock \
+    hypridle \
+    hyprpolkitagent \
+    hyprsunset \
+    rofi-wayland \
+    swww \
+    aylurs-gtk-shell \
+    network-manager-applet \
+    wl-clipboard \
+    brightnessctl \
+    playerctl \
+    kanshi \
+    pavucontrol \
+    blueman
 
 # Cosmic
 curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
