4ba1dbc543c42489d5baef0177a0810deaef14e1
Logan Roberts <logan@lcroberts.dev>
2025-01-05 19:25:57 -0500

    Disable more stuff

--- wmbazzite-gnome/build.sh
+++ wmbazzite-gnome/build.sh
@@ -15,42 +15,41 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" >/etc/yum.repos.d/vscode.repo
 
 # this installs a package from fedora repos
-rpm-ostree --idempotent install \
-    adobe-source-han-sans-jp-fonts \
-    adobe-source-han-serif-jp-fonts \
-    mozilla-fira-mono-fonts \
-    powerline-fonts \
-    jetbrains-mono-fonts-all \
-    google-droid-sans-mono-fonts \
-    zathura \
-    kitty \
-    imv \
-    zathura-plugins-all \
-    fcitx5 \
-    fcitx5-configtool \
-    fcitx5-gtk \
-    fcitx5-qt5 \
-    fcitx5-mozc \
-    fish \
-    neovim \
-    eza \
-    fd-find \
-    ripgrep \
-    zoxide \
-    trash-cli \
-    tmux \
-    iotop \
-    tokei \
-    tealdeer \
-    yq \
-    stow \
-    piper \
-    code \
-    docker-ce \
-    docker-ce-cli \
-    docker-buildx-plugin \
-    docker-compose-plugin \
-    containerd.io
+# rpm-ostree --idempotent install \
+#     adobe-source-han-sans-jp-fonts \
+#     adobe-source-han-serif-jp-fonts \
+#     mozilla-fira-mono-fonts \
+#     powerline-fonts \
+#     jetbrains-mono-fonts-all \
+#     google-droid-sans-mono-fonts \
+#     zathura \
+#     kitty \
+#     imv \
+#     zathura-plugins-all \
+#     fcitx5 \
+#     fcitx5-configtool \
+#     fcitx5-gtk \
+#     fcitx5-qt5 \
+#     fcitx5-mozc \
+#     fish \
+#     neovim \
+#     eza \
+#     fd-find \
+#     ripgrep \
+#     zoxide \
+#     trash-cli \
+#     tmux \
+#     iotop \
+#     tokei \
+#     tealdeer \
+#     yq \
+#     piper \
+#     code \
+#     docker-ce \
+#     docker-ce-cli \
+#     docker-buildx-plugin \
+#     docker-compose-plugin \
+#     containerd.io
 
 # Hyprland Stuff
 # rpm-ostree --idempotent install \
@@ -78,7 +77,6 @@ curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfraclou
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
-systemctl disable btrfs-dedup@var-home.timer
 
 curl -Lo /usr/bin/kmonad https://github.com/kmonad/kmonad/releases/latest/download/kmonad
 chmod +x /usr/bin/kmonad
