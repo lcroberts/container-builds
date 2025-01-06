02d6723b8a782f0def751d0edd3ec4d7e81213dd
Logan Roberts <logan@lcroberts.dev>
2025-01-06 07:43:39 -0500

    Enabel cosmic

--- wmbazzite/build.sh
+++ wmbazzite/build.sh
@@ -80,7 +80,7 @@ rpm-ostree --idempotent install \
 
 # Cosmic
 curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
-# rpm-ostree --idempotent install cosmic-desktop
+rpm-ostree --idempotent install cosmic-desktop
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
