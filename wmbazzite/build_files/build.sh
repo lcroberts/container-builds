cf87bf3d708e0bd35ea4bf3d976d309127ccfc9b
Logan Roberts <logan@lcroberts.dev>
2025-12-26 19:41:38 -0500

    speed up testing and test thing

--- wmbazzite/build_files/build.sh
+++ wmbazzite/build_files/build.sh
@@ -11,63 +11,63 @@ sed -i 's/ID=bazzite/ID=fedora/g' /etc/os-release
 
 . /ctx/functions.sh
 
-dnf5 copr enable -y lcroberts/WMBazzite
-
-uninstall-plasma
-
-install-cosmic
-systemctl enable cosmic-greeter
-
-### Install packages
-
-# GUI/Input/Theming Stuff
-dnf5 install -y \
-    firewall-config \
-    gtk-murrine-engine \
-    kvantum \
-    kvantum-qt5 \
-    qt6ct \
-    qt5ct \
-    zathura \
-    kitty \
-    imv \
-    mpv \
-    mpv-mpris \
-    zathura-plugins-all \
-    fcitx5 \
-    fcitx5-configtool \
-    fcitx5-gtk \
-    fcitx5-qt5 \
-    fcitx5-mozc \
-    piper
-
-dnf5 install -y https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
-
-# CLI Stuff
-dnf5 install -y \
-    nodejs \
-    starship \
-    direnv \
-    eza \
-    neovim \
-    fd-find \
-    ripgrep \
-    zoxide \
-    trash-cli \
-    tmux \
-    iotop \
-    tokei \
-    tealdeer \
-    yq \
-    lazygit
-
-# Misc Stuff
-install-fonts
-install-kmonad
-install-yazi
-
-# Desktops
-install-hyprland
+# dnf5 copr enable -y lcroberts/WMBazzite
+#
+# uninstall-plasma
+#
+# install-cosmic
+# systemctl enable cosmic-greeter
+#
+# ### Install packages
+#
+# # GUI/Input/Theming Stuff
+# dnf5 install -y \
+#     firewall-config \
+#     gtk-murrine-engine \
+#     kvantum \
+#     kvantum-qt5 \
+#     qt6ct \
+#     qt5ct \
+#     zathura \
+#     kitty \
+#     imv \
+#     mpv \
+#     mpv-mpris \
+#     zathura-plugins-all \
+#     fcitx5 \
+#     fcitx5-configtool \
+#     fcitx5-gtk \
+#     fcitx5-qt5 \
+#     fcitx5-mozc \
+#     piper
+#
+# dnf5 install -y https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
+#
+# # CLI Stuff
+# dnf5 install -y \
+#     nodejs \
+#     starship \
+#     direnv \
+#     eza \
+#     neovim \
+#     fd-find \
+#     ripgrep \
+#     zoxide \
+#     trash-cli \
+#     tmux \
+#     iotop \
+#     tokei \
+#     tealdeer \
+#     yq \
+#     lazygit
+#
+# # Misc Stuff
+# install-fonts
+# install-kmonad
+# install-yazi
+#
+# # Desktops
+# install-hyprland
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
